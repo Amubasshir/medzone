087672b810ae172c471cc300069f8fded00a7f26
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-06-17 07:14:04 +0600

    product hero done

---
+++ src/components/ProductHero.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="h-[406px]">
+    <div
+      class="bg-cover bg-center h-full w-full"
+      style="background-image: url('https://i.ibb.co/CBZt1tg/dt-bg.jpg')"
+    >
+      <div class="pl-1 lg:pl-10 px-2 lg:px-44">
+        <div
+          class="flex-col lg:flex lg:flex-row justify-between items-center pt-10 pb-9 lg:pb-16"
+        >
+          <p class="text-white text-xs">
+            Home&nbsp;<span class="font-semibold"> > &nbsp; Our stories</span>
+            <span>
+              &nbsp; > Working to get more people with lung cancer closer to
+              cure</span
+            >
+          </p>
+          <p class="text-white text-[10px] font-light flex items-center">
+            <span>
+              <!-- <svg
+                height="14px"
+                width="14px"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              >
+                <path
+                  d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm0 11h6v1h-7v-9h1v8z"
+                />
+              </svg>  -->
+
+              <img
+                src="../assets/images/home/clockk.svg"
+                alt=""
+                class="w-6 h-6"
+              /> </span
+            ><span class="pl-1">5 Minute Read</span>
+          </p>
+        </div>
+
+        <p class="text-white text-xs tracking-wide pb-5">
+          31 MAY 2023 |
+          <span class="font-bold"> &nbsp; Patients</span>
+        </p>
+        <p
+          class="text-[#fff] text-3xl lg:text-4xl leading-9 lg:leading-[46px] font-light pb-5"
+        >
+          Working to Get More People with Lung Cancer
+          <br class="hidden lg:block" />Closer to a Cure
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -1,11 +1,14 @@
 <script setup>
+import ProductDetails from '../components/ProductDetails.vue';
+import ProductHero from '../components/ProductHero.vue';
 import TheFooter from '../components/TheFooter.vue';
 import TheNavbar from '../components/TheNavbar.vue';
 </script>
 
 <template>
   <TheNavbar />
-
+  <ProductHero />
+  <ProductDetails />
   <TheFooter />
 </template>
 
